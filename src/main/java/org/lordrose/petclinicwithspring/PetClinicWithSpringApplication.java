82d2b13ce7a65dd2f987722c4bda4d8b149c908e
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-14 19:24:10 +0700

    Add sub incremental initial files

---
+++ src/main/java/org/lordrose/petclinicwithspring/PetClinicWithSpringApplication.java
@@ -0,0 +1,13 @@
+package org.lordrose.petclinicwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lordrose/petclinicwithspring/PetClinicWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lordrose.petclinicwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
