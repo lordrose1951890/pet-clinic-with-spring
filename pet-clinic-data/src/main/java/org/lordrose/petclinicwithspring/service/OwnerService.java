37ab2966d3f8bf1676640cbf63a52eed9fcb668c
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-16 19:26:18 +0700

    refactoring services to common interface. Closes #11.

--- pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/OwnerService.java
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/OwnerService.java
@@ -4,13 +4,8 @@ import org.lordrose.petclinicwithspring.model.Owner;
 
 import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Set<Owner> findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/VetService.java
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/VetService.java
@@ -2,14 +2,6 @@ package org.lordrose.petclinicwithspring.service;
 
 import org.lordrose.petclinicwithspring.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
