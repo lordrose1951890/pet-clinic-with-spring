d83f5e75ebbed46ccf3c20307123fb44ea0fe219
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-17 22:36:35 +0700

    refactoring map impl to manage ID incremental generation. Closes #23.

--- pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/map/AbstractMapService.java
@@ -1,13 +1,18 @@
 package org.lordrose.petclinicwithspring.service.map;
 
+import org.lordrose.petclinicwithspring.model.BaseEntity;
+
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
 
-    protected Map<ID, T> map = new HashMap<>();
+    protected Map<Long, T> map = new HashMap<>();
+    private Long indexCounter = null;
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +22,12 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        return map.put(id, object);
+    T save(T object) {
+        setID(object);
+
+        map.put(object.getId(), object);
+
+        return object;
     }
 
     void deleteById(ID id) {
@@ -27,5 +36,35 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        if (isUninitialized(indexCounter))
+            initIndexCounter();
+        return indexCounter++;
+    }
+
+    private void initIndexCounter() {
+        try {
+            indexCounter = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            indexCounter = 1L;
+        }
+    }
+    
+    private boolean isUninitialized(Long indexCounter) {
+        return indexCounter == null;
+    }
+
+    private boolean checkValid(T object) {
+        return object != null;
+    }
+
+    private void setID(T object) {
+        if (checkValid(object)) {
+            object.setId(getNextId());
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
     }
 }
--- pet-clinic-web/src/main/java/org/lordrose/petclinicwithspring/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/lordrose/petclinicwithspring/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Mason");
         owner1.setLastName("Dipper Pines");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Mabel");
         owner2.setLastName("Pines");
 
@@ -45,7 +43,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("## Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Wendy");
         vet1.setLastName("Corduroy");
 
