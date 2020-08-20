e0761165246e2314ea15fa3ef8ae15338a37afc6
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-21 03:19:16 +0700

    adding PetType with boostrap. Closes #34.

--- pet-clinic-web/src/main/java/org/lordrose/petclinicwithspring/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/org/lordrose/petclinicwithspring/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package org.lordrose.petclinicwithspring.bootstrap;
 
 import org.lordrose.petclinicwithspring.model.Owner;
+import org.lordrose.petclinicwithspring.model.PetType;
 import org.lordrose.petclinicwithspring.model.Vet;
 import org.lordrose.petclinicwithspring.service.OwnerService;
+import org.lordrose.petclinicwithspring.service.PetTypeService;
 import org.lordrose.petclinicwithspring.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService,
+                      PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCat = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Mason");
