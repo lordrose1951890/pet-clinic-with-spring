f927a71310980e704cca6eb70ff79425943f5a9f
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-21 02:56:54 +0700

    adding Specialty relating to Vet. Closes #29.

---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/model/Specialty.java
@@ -0,0 +1,14 @@
+package org.lordrose.petclinicwithspring.model;
+
+public class Specialty extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/model/Vet.java
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/model/Vet.java
@@ -1,4 +1,16 @@
 package org.lordrose.petclinicwithspring.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
