0132cf538ec3d260ec031d031e0c8ba0aa3aa51d
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-21 03:10:34 +0700

    adding PetTypeService. Closes #31.

---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/PetTypeService.java
@@ -0,0 +1,7 @@
+package org.lordrose.petclinicwithspring.service;
+
+import org.lordrose.petclinicwithspring.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
