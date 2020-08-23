a5c4c599feee1fe2468d40ad9add3629cdef2132
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-23 22:10:22 +0700

    adding Specialty Map Impl. Closes #33.

---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/SpecialtyService.java
@@ -0,0 +1,6 @@
+package org.lordrose.petclinicwithspring.service;
+
+import org.lordrose.petclinicwithspring.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/map/SpecialtyServiceMap.java
@@ -0,0 +1,37 @@
+package org.lordrose.petclinicwithspring.service.map;
+
+import org.lordrose.petclinicwithspring.model.Specialty;
+import org.lordrose.petclinicwithspring.service.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Specialty, Long>
+        implements SpecialtyService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
