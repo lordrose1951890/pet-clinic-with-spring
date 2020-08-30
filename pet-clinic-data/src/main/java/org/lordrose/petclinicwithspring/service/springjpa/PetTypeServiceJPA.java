fcf728fdb1279c338b9c5ed62e3050fe07a47c52
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-30 21:20:06 +0700

    adding spring jpa service for pet types. Closes #44.

---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/springjpa/PetTypeServiceJPA.java
@@ -0,0 +1,50 @@
+package org.lordrose.petclinicwithspring.service.springjpa;
+
+import org.lordrose.petclinicwithspring.model.PetType;
+import org.lordrose.petclinicwithspring.repository.PetTypeRepository;
+import org.lordrose.petclinicwithspring.service.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springJPA")
+public class PetTypeServiceJPA implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeServiceJPA(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> types = new HashSet<>();
+        petTypeRepository.findAll().forEach(types::add);
+        return types;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        Optional<PetType> result = petTypeRepository.findById(id);
+        return result.orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
+}
