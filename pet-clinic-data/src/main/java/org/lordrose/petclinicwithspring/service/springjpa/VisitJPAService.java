c021bcb26d7084993a6fc2647f76de088a34a464
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-30 23:05:12 +0700

    implementing Spring JPA impl for Visit Service. Closes #49.

---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/springjpa/VisitJPAService.java
@@ -0,0 +1,50 @@
+package org.lordrose.petclinicwithspring.service.springjpa;
+
+import org.lordrose.petclinicwithspring.model.Visit;
+import org.lordrose.petclinicwithspring.repository.VisitRepository;
+import org.lordrose.petclinicwithspring.service.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springJPA")
+public class VisitJPAService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        Optional<Visit> result = visitRepository.findById(id);
+        return result.orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
