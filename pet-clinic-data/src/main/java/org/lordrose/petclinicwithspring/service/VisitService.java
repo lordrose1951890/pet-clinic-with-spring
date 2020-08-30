9d213db78c7dae513e6e648cbca0b114f1c119d8
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-30 22:43:45 +0700

    implementing Map impl for Visit Service. Closes #48.

---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/VisitService.java
@@ -0,0 +1,6 @@
+package org.lordrose.petclinicwithspring.service;
+
+import org.lordrose.petclinicwithspring.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/map/VisitMapService.java
@@ -0,0 +1,53 @@
+package org.lordrose.petclinicwithspring.service.map;
+
+import org.lordrose.petclinicwithspring.model.Visit;
+import org.lordrose.petclinicwithspring.service.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long>
+        implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (isInvalid(visit))
+            throw new RuntimeException("Invalid Visit");
+        return super.save(visit);
+    }
+
+    private boolean isInvalid(Visit visit) {
+        if (visit.getPet() == null) {
+            return true;
+        }
+        if (visit.getPet().getOwner() == null) {
+            return true;
+        }
+        if (visit.getPet().getId() == null) {
+            return true;
+        }
+        if (visit.getPet().getOwner().getId() == null) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
