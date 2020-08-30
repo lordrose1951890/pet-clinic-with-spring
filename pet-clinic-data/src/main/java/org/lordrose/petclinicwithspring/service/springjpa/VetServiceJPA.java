3de159acaa897e544504961da0b840ee6da096fc
DucDH <ducdhse130442@fpt.edu.vn>
2020-08-30 21:15:09 +0700

    adding spring jpa service for vets. Closes #43.

---
+++ pet-clinic-data/src/main/java/org/lordrose/petclinicwithspring/service/springjpa/VetServiceJPA.java
@@ -0,0 +1,50 @@
+package org.lordrose.petclinicwithspring.service.springjpa;
+
+import org.lordrose.petclinicwithspring.model.Vet;
+import org.lordrose.petclinicwithspring.repository.VetRepository;
+import org.lordrose.petclinicwithspring.service.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springJPA")
+public class VetServiceJPA implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetServiceJPA(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        Optional<Vet> result = vetRepository.findById(id);
+        return result.orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
