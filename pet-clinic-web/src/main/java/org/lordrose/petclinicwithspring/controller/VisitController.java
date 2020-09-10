69800f8e8704d57f87bbb20fc9c92d81c77776de
LordRose <lordrose1951980@gmail.com>
2020-09-11 00:18:05 +0700

    adding create visit function. Closes #59.

---
+++ pet-clinic-web/src/main/java/org/lordrose/petclinicwithspring/controller/VisitController.java
@@ -0,0 +1,61 @@
+package org.lordrose.petclinicwithspring.controller;
+
+import org.lordrose.petclinicwithspring.model.Pet;
+import org.lordrose.petclinicwithspring.model.Visit;
+import org.lordrose.petclinicwithspring.service.PetService;
+import org.lordrose.petclinicwithspring.service.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners/{ownerId}/pets/{petId}/visits")
+public class VisitController {
+
+    private final static String VIEWS = "pet/createOrUpdateVisitForm";
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Model model) {
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    @GetMapping("/new")
+    public String getCreateForm(@PathVariable Long petId, Model model) {
+        return VIEWS;
+    }
+
+    @PostMapping("/new")
+    public String processCreateForm(Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return VIEWS;
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
---
+++ pet-clinic-web/src/test/java/org/lordrose/petclinicwithspring/controller/VisitControllerTest.java
@@ -0,0 +1,75 @@
+package org.lordrose.petclinicwithspring.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.lordrose.petclinicwithspring.model.Pet;
+import org.lordrose.petclinicwithspring.service.PetService;
+import org.lordrose.petclinicwithspring.service.VisitService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ExtendWith(MockitoExtension.class)
+class VisitControllerTest {
+
+    @Mock
+    VisitService visitService;
+
+    @Mock
+    PetService petService;
+
+    @InjectMocks
+    VisitController controller;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void setAllowedFields() {
+    }
+
+    @Test
+    void loadPetWithVisit() {
+    }
+
+    @Test
+    void getCreateForm() throws Exception {
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/1/pets/1/visits/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("visit"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pet/createOrUpdateVisitForm"));
+
+        verify(petService).findById(anyLong());
+    }
+
+    @Test
+    void processCreateForm() throws Exception {
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().id(1L).build());
+
+        mockMvc.perform(post("/owners/1/pets/1/visits/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/{ownerId}"));
+
+        verify(visitService).save(any());
+    }
+}
