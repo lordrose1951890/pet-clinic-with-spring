4c89ef8870fa1f2f7a52ed704ab3a34203c72122
LordRose <lordrose1951980@gmail.com>
2020-09-12 21:23:04 +0700

    adding Vet listing function. Closes #66.

--- pet-clinic-web/src/main/java/org/lordrose/petclinicwithspring/controller/VetController.java
+++ pet-clinic-web/src/main/java/org/lordrose/petclinicwithspring/controller/VetController.java
@@ -16,9 +16,7 @@ public class VetController {
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
-
         model.addAttribute("vets", vetService.findAll());
-
         return "vet/index";
     }
 }
